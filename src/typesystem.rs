d227d12086113480cafa4eb43392f9e238615fff
Hugo Lindström <huggepugge1@gmail.com>
2024-12-29 23:55:12 +0100

    better printing of errors

--- src/typesystem.rs
+++ src/typesystem.rs
@@ -23,20 +23,26 @@ impl std::fmt::Display for Type {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Type::Output(o) => match o.status.success() {
-                true => write!(f, "{}", String::from_utf8_lossy(&o.stdout)),
-                false => write!(f, "{}", String::from_utf8_lossy(&o.stderr)),
+                true => write!(f, "{}\n", String::from_utf8_lossy(&o.stdout)),
+                false => write!(f, "{}\n", String::from_utf8_lossy(&o.stderr)),
             },
 
             Type::File(file, path) => color_file(file, path, f),
 
-            Type::String(s) => write!(f, "{}", format!("\"{s}\"").green().to_string()),
-            Type::Array(a) => write!(f, "{}", array_to_string(a).to_string()),
-            Type::Integer(i) => write!(f, "{}", i.to_string().white().to_string()),
-            Type::Float(fl) => write!(f, "{}", fl.to_string().white().to_string()),
-            Type::Boolean(b) => write!(f, "{}", b.to_string().bright_magenta().to_string()),
-            Type::Null => write!(f, "{}", "null".to_string().yellow().to_string()),
+            Type::String(s) => write!(f, "{}\n", format!("\"{s}\"").green().to_string()),
+            Type::Array(a) => write!(f, "{}\n", array_to_string(a).to_string()),
+            Type::Integer(i) => write!(f, "{}\n", i.to_string().white().to_string()),
+            Type::Float(fl) => write!(f, "{}\n", fl.to_string().white().to_string()),
+            Type::Boolean(b) => write!(f, "{}\n", b.to_string().bright_magenta().to_string()),
+            Type::Null => write!(f, "{}\n", "null".to_string().yellow().to_string()),
 
-            Type::Error { message, code } => write!(f, "{} ({})", message.red().to_string(), code),
+            Type::Error { message, code } => write!(
+                f,
+                "{}{}\nExited With status {}\n",
+                "Error: ".red().to_string(),
+                message,
+                code
+            ),
         }
     }
 }
@@ -51,7 +57,7 @@ fn array_to_string(array: &Vec<Type>) -> String {
             s.push_str(",\n");
         }
     }
-    s.push_str("\n]\n");
+    s.push_str("\n]");
     s
 }
 
